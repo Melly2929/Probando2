0110774dd903f610f3271be8cc32bb018562bd96
Rosario <118317818+Melly2929@users.noreply.github.com>
2023-05-30 21:48:46 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let edad 
+ 
+  edad= prompt ("Ingrese su edad");
+
+    if(edad >= 18){
+        alert("podes ingresar :)");
+      }
+      else{
+        alert("Entras bajo tu responsabilidad");}
+      
+
+
+let 
